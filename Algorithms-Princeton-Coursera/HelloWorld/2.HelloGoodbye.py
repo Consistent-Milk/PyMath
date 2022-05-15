89781b362dff125d11fd27100adca75f25bae790
Mahmudul Hasan <mahmudulhasanfh91@gmail.com>
2022-05-15 20:01:34 +0600

    Princeton Commmit 2

---
+++ Algorithms-Princeton-Coursera/HelloWorld/2.HelloGoodbye.py
@@ -0,0 +1,9 @@
+def main():
+    name1 = str(input('Enter First Name: '))
+    name2 = str(input('Enter Second Name: '))
+
+    print(f"Hello {name1} and {name2}.")
+    print(f"Goodbye {name2} and {name1}")
+
+if __name__ == "__main__":
+    main()
